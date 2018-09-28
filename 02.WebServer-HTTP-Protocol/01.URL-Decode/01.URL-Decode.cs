d30e017b4e91f82b7976f450a5c22bcd2f7ec620
radoslavvv <radoslav.varadinov@gmail.com>
2018-09-29 02:26:16 +0300

    added progress to second homework

---
+++ 02.WebServer-HTTP-Protocol/01.URL-Decode/01.URL-Decode.cs
@@ -0,0 +1,22 @@
+﻿namespace _02.WebServer_HTTP_Protocol
+{
+    using System;
+    using System.Net;
+
+    public class URLDecoder
+    {
+        public static void Main()
+        {
+            string encodedURL = Console.ReadLine();
+            string decodedURL = DecodeURL(encodedURL);
+
+            Console.WriteLine(decodedURL);
+        }
+
+        private static string DecodeURL(string encodedUrl)
+        {
+            string decodedURL = WebUtility.UrlDecode(encodedUrl);
+            return decodedURL;
+        }
+    }
+}
---
+++ 02.WebServer-HTTP-Protocol/02.ValidateURL/02.ValidateURL.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace _02.ValidateURL
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string decodedURL = WebUtility.UrlDecode(input);
+
+            Regex validURLregex = new Regex(@"(?<protocol>http|https):\/\/(www\.)*(?<host>[A-Za-z-]+)\.(?<domain>\w+)(?<port>:\d+)*(?<path>.+)*");
+
+            Regex queryRegex = new Regex(@"(?<path>.+)\?(?<query>.+)(?<fragment>#.*)*");
+
+            Regex fragmentRegex = new Regex(@"(?<fragment>#.*)");
+
+            if (validURLregex.IsMatch(input))
+            {
+                Console.WriteLine("yes");
+                Match match = validURLregex.Match(input);
+
+                string protocol = match.Groups["protocol"].Value;
+                string host = match.Groups["host"].Value;
+                string domain = match.Groups["domain"].Value;
+                string port = match.Groups["port"].Value;
+                string path = match.Groups["path"].Value;
+               
+                if (host == "https" && port != "443" || host == "http" && port != "80")
+                {
+                    Console.WriteLine("Invalid URL");
+                }
+                else
+                {
+                    Console.WriteLine($"Protocol: {protocol}");
+                    Console.WriteLine($"Host: {host}.{domain}");
+
+                    string defaultPort = protocol == "https" ? "443" : "80";
+                    Console.WriteLine($"Port: {defaultPort}");
+
+                    if (queryRegex.IsMatch(path))
+                    {
+                        Match queryMatch = queryRegex.Match(path);
+                        string query = queryMatch.Groups["query"].Value;
+                        string queryPath = queryMatch.Groups["path"].Value;
+
+                        Console.WriteLine($"Path: {queryPath}");
+                        Console.WriteLine($"Query: {query}");
+
+                        if(fragmentRegex.IsMatch(query))
+                        {
+                            Match fragmentMatch = fragmentRegex.Match(query);
+                            string fragment = fragmentMatch.Groups["fragment"].Value;
+                            //TODO:
+
+                            Console.WriteLine($"Fragment: {fragment}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Path: {path}");
+                    }
+                }
+            }
+        }
+    }
+}
