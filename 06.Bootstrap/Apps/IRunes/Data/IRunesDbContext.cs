60725786af43150cfbe525dd1bf89eccde29696a
radoslavvv <radoslav.varadinov@gmail.com>
2018-10-13 21:26:18 +0300

    added solution for 06.IRunes-Bootstrap

---
+++ 06.Bootstrap/Apps/IRunes/Data/IRunesDbContext.cs
@@ -0,0 +1,26 @@
+﻿using IRunes.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IRunes.Data
+{
+    public class IRunesDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Album> Albums { get; set; }
+
+        public DbSet<Track> Tracks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=KING\\SQLEXPRESS; Database=IRunes3; Integrated Security=true");
+
+            optionsBuilder.UseLazyLoadingProxies();
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ 06.Bootstrap/SIS.HTTP/Sessions/IHttpSession.cs
@@ -0,0 +1,16 @@
+﻿namespace SIS.HTTP.Sessions
+{
+    public interface IHttpSession
+    {
+        string Id { get; }
+
+        object GetParameter(string name);
+
+        bool ContainsParameter(string name);
+
+        void AddParameter(string name, object parameter);
+
+        void ClearParameters();
+        void RemoveParameter(string v);
+    }
+}
